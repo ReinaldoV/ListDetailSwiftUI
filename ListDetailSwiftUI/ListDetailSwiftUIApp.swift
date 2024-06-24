b520ef615711cd6350a6bfcaa6b39d70bda266cb
ReinaldoV <reinaldovillajavi@gmail.com>
2024-06-24 09:55:37 +0200

    inital commit

---
+++ ListDetailSwiftUI/ListDetailSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListDetailSwiftUIApp.swift
+//  ListDetailSwiftUI
+//
+//  Created by Villanueva, Reinaldo on 24/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ListDetailSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
